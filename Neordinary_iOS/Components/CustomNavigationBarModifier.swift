fa01a4fc78cc19cc630271458549dc03057fa19b
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-05-18 04:39:36 +0900

    fix: custom navigation bar modifier

--- Neordinary_iOS/Components/CustomNavigationBarModifier.swift
+++ Neordinary_iOS/Components/CustomNavigationBarModifier.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct CustomNavigationBarModifier<C, L, R> : ViewModifier where C : View, L : View, R : View {
+    let centerView: (() -> C)?
+    let leftView: (() -> L)?
+    let rightView: (() -> R)?
+    
+    init(centerView: (() -> C)? = nil, leftView: (() -> L)? = nil, rightView: (() -> R)? = nil) {
+        self.centerView = centerView
+        self.leftView = leftView
+        self.rightView = rightView
+    }
+    func body(content : Content) -> some View {
+        VStack {
+            ZStack {
+                HStack {
+                    self.leftView?()
+                    Spacer()
+                    self.rightView?()
+                }
+                .frame(height: 46.0)
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 20)
+                        
+                HStack {
+                    Spacer()
+                    
+                    self.centerView?()
+                    
+                    Spacer()
+                }
+            }
+            .background(Color.clear)
+            
+           content
+
+            Spacer()
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
