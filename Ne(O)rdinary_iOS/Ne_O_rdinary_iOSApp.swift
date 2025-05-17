4df76da88c5a8da2eb5485ef16c30aa5985a9f9a
jwon0523 <jwon010523@gmail.com>
2025-05-17 15:21:50 +0900

    init: 초기 세팅

---
+++ Ne(O)rdinary_iOS/Ne_O_rdinary_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ne_O_rdinary_iOSApp.swift
+//  Ne(O)rdinary_iOS
+//
+//  Created by jaewon Lee on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Ne_O_rdinary_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
