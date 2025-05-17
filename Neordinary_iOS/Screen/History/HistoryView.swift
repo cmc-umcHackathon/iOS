6a5bef50cc187266e57f00fd926bea894459cb7f
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-05-18 04:38:54 +0900

    add: image asset

---
+++ Neordinary_iOS/Screen/History/HistoryView.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryView.swift
+//  Neordinary_iOS
+//
+//  Created by 남경민 on 5/18/25.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .custom
+    }
+}
+
+#Preview {
+    HistoryView()
+}
