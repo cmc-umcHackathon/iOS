833e966bd2cdcda63cc149510462ec11da07925e
jwon0523 <jwon010523@gmail.com>
2025-05-17 20:24:40 +0900

    init: xcodegen 도입 위한 프로젝트 초기화

---
+++ Neordinary_iOS/Neordinary_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neordinary_iOSApp.swift
+//  Neordinary_iOS
+//
+//  Created by jaewon Lee on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Neordinary_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
