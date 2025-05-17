8ce8863d9be1ba6f99b9697596eca3a8e82cf7d7
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-05-18 01:15:08 +0900

    feat: my page view

---
+++ Neordinary_iOS/Screen/MyPage/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  Neordinary_iOS
+//
+//  Created by 남경민 on 5/18/25.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
